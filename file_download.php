05841f04afcba831d6ef8475edc6c959f97c6e70
[KBoohene] <[okwabse@gmail.com]>
2017-01-22 18:14:38 +0000

    Implemented basic sending file from android device and storing in a database

--- file_download.php
+++ file_download.php
@@ -1,24 +1,20 @@
 <?php
 
-/*if (is_uploaded_file($_FILES['fileToUpload']['tmp_name'])) {
-   echo "File ". $_FILES['fileToUpload']['name'] ." uploaded successfully.\n";
 
-} else {
-   echo "file failed to upload";
-}*/
-
-/*$fileDir ="coordinates/";
-$uploadfile = $fileDir . basename($_FILES['fileToUpload']['name']);
-if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
-    echo "Successful";
-} else {
-    echo "Possible file upload failed";
-}
-*/
-*
 include_once("coordinates.php");
 $obj = new coordinates();
 
+$fileDir ="coordinates/";
 
+$uploadfile = $fileDir . basename($_FILES['fileToUpload']['name']);
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
+    echo "Successful";
+    $obj->read($uploadfile);
+
+    //Deletes file from directory
+    //$unlink($uploadfile);
+} else {
+    echo "Possible file upload failed";
+}
 
 ?>
