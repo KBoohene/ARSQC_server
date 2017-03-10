d1606b07581046a59b4df791f807b9c1905c25ce
[KBoohene] <okwabse@gmail.com>
2017-03-10 19:45:34 +0000

    Uncommented GPS data

--- QuadtreeTest.php
+++ QuadtreeTest.php
@@ -9,7 +9,7 @@
   </head>
   <body>
    <?php
-    //include_once("coordinates.php");
+    include_once("coordinates.php");
     /*
     0 takes GPS longitude
     1 takes GPS latitude
@@ -17,20 +17,18 @@
     3 takes Next Longitude
     4 takes Next Latitude
     */
-      /*$GPSdata = array(array());
+      $GPSdata = array(array());
       $obj = new coordinates();
       $coordData =$obj->fetchAllData();
       $count=0;
       while($coordData->fetch()){
 
-        $GPSdata[$count][0]=coordData['grade'];
-        $GPSdata[$count][1]=coordData['Longitude'];
-        $GPSdata[$count][2]=coordData['Latitude'];
-        $GPSdata[$count][3]=coordData['nxtLongitude'];
-        $GPSdata[$count][4]=coordData['nxtLatitude'];
-        $GPSdata[$count][5]=coordData['routeId'];
-
-      */
+        $GPSdata[$count][0]=$coordData['grade'];
+        $GPSdata[$count][1]=$coordData['Longitude'];
+        $GPSdata[$count][2]=$coordData['Latitude'];
+        $GPSdata[$count][3]=$coordData['nxtLongitude'];
+        $GPSdata[$count][4]=$coordData['nxtLatitude'];
+        $GPSdata[$count][5]=$coordData['routeId'];
     ?>
     <script>
       var set = new GeoTree();
@@ -39,7 +37,7 @@
       /*var GPSpoints = <?php //echo json_encode($GPSdata); ?>;
       for(int i;i<=GPSpoints.length;i++){
         GradeData =[NxtLng:GPSpoints[i][3], NxtLat:GPSpoints[i][4], Grade:GPSpoints[i][0]];
-        binaryTreeGPS=[lat:GPSpoints[i][1], lng:GPSpoints[i][2], data:GradeData];
+        binaryTreeGPS=[lat:GPSpoints[i][2], lng:GPSpoints[i][1], data:GradeData];
         tempArray.push(binarryTreeGPS);
       }
       set.insert(tempArray);
