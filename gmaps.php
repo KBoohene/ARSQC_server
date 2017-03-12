3ce5aa6dc6ee097f0393d4d384eac8a4f52b85f0
[KBoohene] <okwabse@gmail.com>
2017-03-12 11:08:37 +0000

    Merged google maps page and geotree datastructure

--- gmaps.php
+++ gmaps.php
@@ -1,15 +1,10 @@
 <!doctype html>
 <html>
   <head>
-    <style>
-      #map {
-        width: 1000px;
-        height: 500px;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="map"></div>
+    <!--Scripts needed for geotree datastructure-->
+    <script src="geo-tree/z-curve.js" type="text/javascript"></script>
+    <script src="geo-tree/red-black.js" type="text/javascript"></script>
+    <script src="geo-tree/geo-tree.js" type="text/javascript"></script>
 
     <!-- Google Maps JS API -->
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDVy-fqmHDhDqCo3N9MQFknESI5mbzOepU"></script>
@@ -18,8 +13,58 @@
     <script src="calLatLng.js"></script>
     <script src="calDistance.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-    <script>
+  </head>
+  <body>
+    <!--Google maps display-->
+    <div id="map" style="width:1000px; height:500px;"></div>
 
+    <!--This section loads data from the database into the geotree datastructure-->
+    <?php
+    include_once('coordinates.php');
+    /*
+    0 takes Road Grade
+    1 takes GPS longitude
+    2 takes GPS latitude
+    3 takes Next Longitude
+    4 takes Next Latitude
+    */
+
+    $GPSdata = array(array());
+    $obj = new coordinates();
+    $obj->fetchAllData();
+    $count=0;
+
+    //Stores data from database in a 2d array
+    while($coordData=$obj->fetch()){
+      $GPSdata[$count][0]=$coordData['grade'];
+      $GPSdata[$count][1]=$coordData['Longitude'];
+      $GPSdata[$count][2]=$coordData['Latitude'];
+      $GPSdata[$count][3]=$coordData['nxtLongitude'];
+      $GPSdata[$count][4]=$coordData['nxtLatitude'];
+      $GPSdata[$count][5]=$coordData['routeId'];
+      $count++;
+    }
+
+    ?>
+    <script>
+      var set = new GeoTree();
+      var binaryTreeGPS=[], GradeData=[], tempArray=[];
+
+      //Stores php 2d array in javascript
+      var GPSpoints = <?php echo json_encode($GPSdata); ?>;
+
+      //Dumps data into geotree datastructure
+      for (var i=0;i<GPSpoints.length;i++){
+        GradeData={NxtLng:parseFloat(GPSpoints[i][3]),NxtLat:parseFloat(GPSpoints[i][4]),grade:GPSpoints[i][0]};
+        binaryTreeGPS={lat: parseFloat(GPSpoints[i][2]),lng: parseFloat(GPSpoints[i][1]),data: GradeData};
+        //console.log(binaryTreeGPS);
+        set.insert(binaryTreeGPS);
+      }
+      set.dump();
+    </script>
+
+    <!--This section deals with all aspects of the map interface-->
+    <script>
         /* Map Object */
         var mapObj = new GMaps({
           zoom:7,
@@ -36,6 +81,7 @@
         var markers =[];
         var markerCounter;
 
+        //Finds the user's geolocation
         $("#pos-link").click(function(event) {
             mapObj.removeMarkers();
             GMaps.geolocate({
@@ -65,6 +111,7 @@
           });
         });
 
+        //Submits users search request
         $("#submit").click(function() {
             GMaps.geocode({
               address: $('#to').val(),
@@ -91,6 +138,7 @@
 
           });
 
+      //Draws rectangular boundary around two points
       function drawBoundary(destiLat,destiLng){
           var sourceVertical, sourceHorizontal=[];
           var destiVertical, destiHorizontal=[];
@@ -166,40 +214,14 @@
             SE[0] = destiVertical[0];
             SE[1] = sourceVertical[1];
           }
-        //console.log(destiHorizontal[0]+" "+destiVertical[1]);
-        //console.log(sourceHorizontal[0]+" "+sourceVertical[1]);
 
-          /*//Northwest Marker
-          mapObj.addMarker({
-            lat: NW[0],
-            lng: NW[1]
-          });
-
-          //North East Marker
-          mapObj.addMarker({
-              lat: NE[0],
-              lng: NE[1]
-            });
-
-          //South West Marker
-           mapObj.addMarker({
-              lat: SW[0],
-              lng: SW[1]
-            });
-
-          //South East Marker
-          mapObj.addMarker({
-            lat:SE[0],
-            lng:SE[1]
-          });
-          */
           var path = [[NW[0],NW[1]],
                       [NE[0],NE[1]],
                       [SE[0],SE[1]],
                       [SW[0],SW[1]]];
 
             polygon = mapObj.drawPolygon({
-              paths: path, // pre-defined polygon shape
+              paths: path,
               strokeColor: '#BBD8E9',
               strokeOpacity: 1,
               strokeWeight: 3,
@@ -219,13 +241,13 @@
 
     </script>
 
+    <!--Form to take in user search data-->
     <form id="draw-route" name="draw-route" action="#" method="get">
       <label for="to">To:</label>
       <input type="text" id="to" name="to" required="required" placeholder="Another address" size="30" />
       <a id="pos-link" href="#">Get my position</a>
       <br />
       <button id="submit" type="button">Submit</button>
-      <!--<button id="draw" type="button">Draw</button>-->
     </form>
 
 
