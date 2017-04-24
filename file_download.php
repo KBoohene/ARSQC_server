98b78da562ff9ea0c4e322cf63d76a39d5958e68
[KBoohene] <okwabse@gmail.com>
2017-04-24 14:22:28 +0000

    Solved sending of data from phone to database

--- file_download.php
+++ file_download.php
@@ -9,27 +9,24 @@ include_once("coordinates.php");
 $obj = new coordinates();
 
 //Directory of stored coordinates on the server
+$fileDir ="coordinates/";
 
-//$uploadfile = $fileDir . basename($_FILES['fileToUpload']['name']);
+$uploadfile = $fileDir . basename($_FILES['fileToUpload']['name']);
+
+//Moves file from temp folder into final directory
+if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
+    echo "Successful";
+} else {
+    echo "Possible file upload failed";
+}
+
+
 $files =  glob("coordinates/*.txt");
-
-
 
 foreach($files as $file) {
 	$obj->readFile($file);
+	//unlink($file);
 }
-//$obj->insertData("route");
 
-/*//Moves file from temp folder into final directory
-if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
-    echo "Successful";
 
-    //Read file data into database
-    $obj->read($uploadfile);
-
-    //Deletes file from directory
-    //$unlink($uploadfile);
-} else {
-    echo "Possible file upload failed";
-}*/
 ?>
