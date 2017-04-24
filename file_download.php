12d6133ef7736c0615f9dc99737fc7dced00fcd9
[KBoohene] <okwabse@gmail.com>
2017-04-24 21:07:52 +0000

    added delete file functionality

--- file_download.php
+++ file_download.php
@@ -10,23 +10,28 @@ $obj = new coordinates();
 
 //Directory of stored coordinates on the server
 $fileDir ="coordinates/";
-
 $uploadfile = $fileDir . basename($_FILES['fileToUpload']['name']);
 
 //Moves file from temp folder into final directory
 if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile)) {
-    echo "Successful";
+   echo "Successful";
 } else {
     echo "Possible file upload failed";
 }
 
 
+if(isset($_GET['confirm'])){
+
 $files =  glob("coordinates/*.txt");
 
-foreach($files as $file) {
+	foreach($files as $file) {
 	$obj->readFile($file);
-	//unlink($file);
-}
+	}
 
+	foreach($files as $file){
+		unlink($file);
+	}
+
+}
 
 ?>
